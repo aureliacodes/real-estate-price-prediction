58ef55db1188b0ce86a5991f1e5191241cd1ea8d
aureliacodes <aureliacucereavii@hotmail.com>
2025-05-05 01:15:33 +0300

    ^[[[

--- scripts/insert_data.py
+++ scripts/insert_data.py
@@ -2,15 +2,17 @@ import pandas as pd
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
 import os
-import sys
+
 db_url = None
 load_dotenv() 
+
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 DB_NAME = os.getenv("DB_NAME")
 DB_PORT = int(os.getenv("DB_PORT", 3306))
 DATABASE_URL = os.getenv("DATABASE_URL")
+
 if DATABASE_URL:
     db_url = DATABASE_URL 
 else:
@@ -27,22 +29,25 @@ df = df.rename(columns={
     'KitchenAbvGr': 'Kitchen',
 })
 
-# Replace NaN values with 0 
+# Replace NaN values with 0
 df = df.fillna({
     col: 0 if df[col].dtype in ['float64', 'int64'] else 'Unknown'
     for col in df.columns
 })
+print(df.columns)
 
-# Create the conection engine to MySQL
 
+df = df.drop(columns=['Id'])  
+
+# Create a SQLAlchemy engine
 engine = create_engine(db_url, connect_args={"connect_timeout": 15})
+
 if engine.dialect.has_table(engine.connect(), 'properties'):
-    print(" Table already exists — inserting rows.")
+    print("Table already exists — inserting rows.")
 else:
     print("Table does not exist. Please create it before inserting.")
 
 
-#Load directly into MySQL (if you want to add the data without deleting the table, use if_exists='append')
 df.to_sql('properties', con=engine, if_exists='append', index=False)
 
 print("Data inserted successfully with SQLAlchemy!")
